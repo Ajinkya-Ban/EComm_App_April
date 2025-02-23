395914905b81d44ea2abbce80508191a45a2a5e4
Ajinkya-Ban <ajinkyaban@gmail.com>
2025-02-23 14:38:24 +0530

    all code and private routing code deployed

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import HomePage from "./pages/HomePage";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+import Policy from "./pages/Policy";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/Dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route path="Dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/policy" element={<Policy />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
